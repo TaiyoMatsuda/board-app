db01bad81cc863861146ede3e24b7071f7c9dd67
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2020-09-07 23:11:46 +0000

    manage.pyの階層修正、flake8に合わせてファイルを修正

--- src/app/settings.py
+++ src/app/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -86,6 +86,9 @@ DATABASES = {
         'PASSWORD': 'password',
         'HOST': 'db',
         'PORT': 3306,
+        'TEST': {
+            'NAME': 'test_database'
+        }
     }
 }
 
@@ -95,16 +98,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation \
+        .UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation \
+        .MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation \
+        .CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation \
+        .NumericPasswordValidator',
     },
 ]
 
