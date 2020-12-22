15846f02643467c6d732e2e2a9f6694467e4b0f9
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2020-12-22 14:06:31 +0000

    test

--- src/app/settings.py
+++ src/app/settings.py
@@ -94,7 +94,18 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': env.db()
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'board',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+    }
+    #'default': env.db()
 }
 # if '/code/.tox/py38/bin/pytest' in sys.argv:
 #     DATABASES['default'] = {
