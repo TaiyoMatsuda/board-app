0d7f634ab210709ea2dc9c3ea332a23a05263da7
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2021-01-04 23:56:47 +0900

    edit databases of debug on settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -89,13 +89,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': env.db()
-}
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     # }
+#     'default': env.db()
+# }
 
 if '/code/.tox/py38/bin/pytest' in sys.argv:
     DATABASES = {
