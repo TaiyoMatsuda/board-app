0724b011efd9a61b946002ef10e09cfd9339cd7a
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2021-01-04 20:59:40 +0900

    edit databases of debug on settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -111,7 +111,7 @@ if '/code/.tox/py38/bin/pytest' in sys.argv:
         }
     }
 
-if os.environ.get('CIRCLECI'):
+if env.get_value('CIRCLECI', default=True):
     DATABASES = {
         'default': {
             'ENGINE': str(env.get_value('MYSQL_ENGINE')),
